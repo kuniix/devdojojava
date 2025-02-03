a375ebdf24f1508518dfd5325bf6ea8353df9b76
Lucas <42320754+kuniix@users.noreply.github.com>
2025-02-03 09:11:02 -0300

    Metodo toString

---
+++ src/devdojo/sobrescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package devdojo.sobrescrita.test;
+
+import devdojo.sobrescrita.domain.Anime;
+
+public class AnimeTest01 extends Object {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Kaiju");
+
+        System.out.println(anime);
+    }
+}
